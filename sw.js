d907587faaa23f8080d070980237b921b7a1af0a
MB686 <101170940+MB686@users.noreply.github.com>
2025-10-02 16:17:27 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,23 @@
+const CACHE_NAME = 'ww-diary-cache-v1';
+const urlsToCache = [
+    '/points-calculator-beta/',
+    '/points-calculator-beta/index.html',
+    '/points-calculator-beta/log.html',
+    '/points-calculator-beta/weight.html',
+    '/points-calculator-beta/icon.png',
+    '/points-calculator-beta/styles.css'
+];
+
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open('points-calculator-beta-cache-v1').then(cache => {
-            return cache.addAll([
-                '/points-calculator-beta/',
-                '/points-calculator-beta/index.html',
-                '/points-calculator-beta/log.html',
-                '/points-calculator-beta/weight.html',
-                '/points-calculator-beta/icon.png'
-            ]);
-        })
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
     );
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
     );
 });
